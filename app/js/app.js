8e94de93e07256c037485eb7031b0d862fdbfabd
Owen <owen@howtank.com>
2020-11-05 18:19:58 +0100

    added scorecount

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    
+ 
     if(navigator.geolocation) {
       console.log('Geolocalisation OK')
       navigator.geolocation.getCurrentPosition(success);
@@ -8,16 +8,35 @@ window.addEventListener("DOMContentLoaded", (event) => {
       alert('Votre Geolocalisation est inactive')
     }
   });
+  let a = 0;
   const header=document.querySelector('header')
   const positionHolder= document.createElement("div");
+  const scoreText = document.createTextNode(`Score : ${a}`);
+  const scoreHolder = document.createElement('p');
+  
+  scoreHolder.innerHTML = a;
+  header.appendChild(scoreHolder)
   function success(pos) {
     const crd = pos.coords;
-    const positionLat = document.createTextNode(`Latitude : ${crd.latitude}`);
-    const positionLong = document.createTextNode(`Longitude : ${crd.longitude}`);
-    header.appendChild(positionLat)
-    header.appendChild(positionLong)
+    const positionLat = `Latitude : ${crd.latitude}`;
+    const positionLong = `Longitude : ${crd.longitude}`;
+    const positionLatHolder = document.createElement('p');
+    const positionLongHolder = document.createElement('p');
+    positionLatHolder.innerHTML = positionLat;
+    positionLongHolder.innerHTML = positionLong;
+    header.appendChild(positionLatHolder)
+    header.appendChild(positionLongHolder)
     const latitude = crd.latitude.toString();
     const longitude = crd.longitude.toString();
+
+    function updateData(positionLatHolder,positionLongHolder){
+      scoreHolder.innerHTML = a;
+      positionLatHolder.innerHTML = positionLat;
+      positionLongHolder.innerHTML = positionLong;
+      console.log(updatedData)
+    }
+  
+    setInterval(function(){ updateData(); }, 100);
 
     const mymap = L.map('mapid').setView([ latitude, longitude ], 13);
     var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -39,8 +58,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
     L.marker([ latitude, longitude ], {icon: gpsIcon}).addTo(mymap);
 
-    const myRequest = new Request('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&rows=5000&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=circonferenceencm&facet=hauteurenm&facet=remarquable&geofilter.distance=48.8512725%2C+2.3800521%2C+5000');
 
+    const myRequest = new Request('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&rows=5000&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=circonferenceencm&facet=hauteurenm&facet=remarquable&geofilter.distance=48.8512725%2C+2.3800521%2C+1000');
+    
+  
   fetch(myRequest)
     .then(response => response.json())
     .then(data => {
@@ -48,7 +69,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
         const latitudeTree = tree.fields.geo_point_2d[0];
         const longitudeTree = tree.fields.geo_point_2d[1];
         L.marker([ latitudeTree, longitudeTree ], {icon: treeIcon}).addTo(mymap);
+        var circle = L.circle([ latitudeTree, longitudeTree ], {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.1,
+          radius: 500
+      }).addTo(mymap);
       }
+      
     });
+
+    
   }
- 
+  
+  //SCORE STORAGE
+  
+  const buttonTest = document.querySelector("#button-test")
+  buttonTest .addEventListener("click", AddScore)
+  
+  function AddScore(){
+    var aUpdated = a++;
+    console.log(aUpdated)
+    localStorage.setItem('Score', a );
+    scoreHolder.innerHTML = a
+  }
+
+  
