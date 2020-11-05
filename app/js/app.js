0220349dcb6ec9c0c6de348d8f6e991d611ce4c0
Owen <owen@howtank.com>
2020-11-05 14:45:38 +0100

    adding gps icon to repo

--- app/js/app.js
+++ app/js/app.js
@@ -17,12 +17,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const latitude = crd.latitude.toString();
     const longitude = crd.longitude.toString();
     const mymap = L.map('mapid').setView([ latitude, longitude ], 13);
-    var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { // LIGNE 20
-            attribution: '© OpenStreetMap contributors',
-            maxZoom: 19
-        });
-    
-        mymap.addLayer(osmLayer);
-    console.log(mymap)
+   
+    var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+      maxZoom: 19
+    });
+    mymap.addLayer(osmLayer);
+    var treeIcon = L.icon({
+      iconUrl: '../app/src/46564.svg',
+      iconSize:     [38, 95], // size of the icon
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    var gpsIcon = L.icon({
+      iconUrl: '../app/src/location-pointer.svg',
+      iconSize:     [38, 95], // size of the icon
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    L.marker([ latitude, longitude ], {icon: gpsIcon}).addTo(mymap);
+        
   }
  
