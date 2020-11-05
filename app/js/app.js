ef249fba8c33a70a06ca29ef307880a6a42754ab
Owen <owen@howtank.com>
2020-11-05 16:47:46 +0100

    fixing points on map

--- app/js/app.js
+++ app/js/app.js
@@ -17,7 +17,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const latitude = crd.latitude.toString();
     const longitude = crd.longitude.toString();
     const mymap = L.map('mapid').setView([ latitude, longitude ], 13);
-   
     var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
       maxZoom: 19
@@ -25,7 +24,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     mymap.addLayer(osmLayer);
     var treeIcon = L.icon({
       iconUrl: '../app/src/46564.svg',
-      iconSize:     [38, 95], // size of the icon
+      iconSize:     [10, 10], // size of the icon
       iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
       popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
@@ -37,18 +36,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
     L.marker([ latitude, longitude ], {icon: gpsIcon}).addTo(mymap);
 
-    const myRequest = new Request('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&facet=typeemplacement&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=genre&facet=espece&facet=varieteoucultivar&facet=circonferenceencm&facet=hauteurenm&facet=stadedeveloppement&facet=remarquable');
+    const myRequest = new Request('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&rows=5000&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=circonferenceencm&facet=hauteurenm&facet=remarquable&geofilter.distance=48.8512725%2C+2.3800521%2C+1000');
 
   fetch(myRequest)
     .then(response => response.json())
     .then(data => {
       for (const tree of data.records) {
-        console.log(tree)
         const latitudeTree = tree.fields.geo_point_2d[0];
         const longitudeTree = tree.fields.geo_point_2d[1];
         L.marker([ latitudeTree, longitudeTree ], {icon: treeIcon}).addTo(mymap);
       }
-  });
+    });
 
   }
  
